fa84990b4acebc88cf8e06105d4dd49e78d7c427
Anthony Nolan <anthonynolan@gmail.com>
2022-03-18 18:46:30 +0000

    Added scoreboard

--- game.py
+++ game.py
@@ -1,17 +1,29 @@
 import sys, pygame
+import pickle
 
 pygame.init()
 
-size = width, height = 800, 600
+size = width, height = 1480, 700
 
 speed = [1, 1]
+speed2 = [2,-2]
+
 black = 0,0,0
 
 screen = pygame.display.set_mode(size)
 
 
+hound = pygame.image.load('resources/little-hound.jpg')
 ball = pygame.image.load('resources/little-hound.jpg')
+
+hound = pygame.transform.scale(hound, (50,50))
+houndrect = hound.get_rect()
 ballrect = ball.get_rect()
+ballrect.center = (150,100)
+
+font = pygame.font.Font('freesansbold.ttf', 32)
+
+houndrect.center = (400, 400)
 
 begin = pygame.time.get_ticks()
 
@@ -20,58 +32,84 @@ delay = 5
 
 bg = pygame.image.load('resources/family.png')
 
+import os
+filename   = 'scoreboard.pkl'
+if os.path.isfile(filename):
+	scoreboard = pickle.load(open(filename, 'rb'))
+	print(sorted(scoreboard, reverse=True))
+else:
+	scoreboard = []
+
+
+
 def end_game():
 	
 	print('GAME OVER')
 	speed_change()
+	scoreboard.append(score)
+	scoreboard.sort(reverse=True)
+	print(f'you are in position {scoreboard.index(score)} out of {len(scoreboard)}')
 	print(f'final score {score:,}')
+	     
+	pickle.dump(scoreboard, open(filename, 'wb'))
+
 	sys.exit()        
 
 def speed_change():
 	pygame.mixer.Sound('resources/dog.mp3').play()   
-	global begin
-	global score
+	global speed
 	global delay
-	end = pygame.time.get_ticks()
-	elapsed = end-begin
-	score = score+(elapsed*((10-delay)**2))
 	delay = delay-1
-	begin = end
-	print(f'speed is {10-delay}')
+	speed[0] = speed[0] *1.1
+	speed[1] = speed[1]*1.1
 
 
 while True:
 	for event in pygame.event.get():
 		if event.type ==pygame.QUIT: sys.exit()
 		if event.type ==pygame.KEYDOWN:
-			if event.unicode=='p':
+			if event.unicode=='p' or event.key==1073741903:
 				# righto
 				speed[0] = 1
-			elif event.unicode=='o':
+			elif event.unicode=='o' or event.key==1073741904:
 				# left
 				speed[0] = -1
-			elif event.unicode=='q':
-				# up
-				speed[1] = -1
-			elif event.unicode=='a':
-				# down
+			elif event.unicode=='q' or event.key==1073741906:
+				# up 
+				speed[1]  = -1
+			elif event. unicode=='a' or event.key==1073741905:
+				# down 
 				speed[1] = 1
-			elif event.key==32:
-				speed_change()
+			elif event.key==32:         
+				speed_change() 
 				
 
-	ballrect = ballrect.move(speed)
-	if ballrect.left<0 or ballrect.right > width:
-		speed[0] = -speed[0]
+	houndrect = houndrect.move(speed)
+	if houndrect.left<0 or houndrect.right > width:
 		end_game()
-	if ballrect.top < 0 or ballrect.bottom> height:
-		speed[1] = -speed[1]
+	if houndrect.top < 0 or houndrect.bottom> height:
 		end_game()
+
+	ballrect = ballrect.move(speed2)
+	if ballrect.left < 0 or ballrect.right > width:
+		speed2[0] = -speed2[0] 
+	if ballrect.top < 0 or ballrect.bottom > height:
+		speed2[1] = -speed2[1]
 
 	screen.fill(black)
-	screen.blit(bg, (0, 0))
+	# screen.blit(bg, (0, 0))
+	screen.blit(hound, houndrect)
 	screen.blit(ball, ballrect)
+	if houndrect.colliderect(ballrect):
+   		end_game()
 
+	end = pygame.time.get_ticks()
+	elapsed = end-begin
+	score = (elapsed*((6-delay)**2))
+	
+	sc = font.render(str(score), True, (255,255,255))
+	screen.blit(sc, (20,20)) 
 	pygame.display.flip()
+
 	pygame.time.wait(delay)
 
